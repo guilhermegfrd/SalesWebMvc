ff9401150565eeb33d321428fce2c9f13c41489c
Guilherme Fernandes <guilherme.gfrd@gmail.com>
2020-05-09 15:11:01 -0300

    First controller and Razor pages test

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web App from C# Course";
+            ViewData["Productor"] = "Guilherme Duarte";
 
             return View();
         }
